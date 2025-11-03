ee1b792e076f094e1846b6d1a668ac3122b32dc7
Umoy <umoy85@live.de>
2025-11-03 21:51:54 +0100

    L4M3 _ Timeline needs to be extended

--- Assets/Code/CharacterAnim.cs
+++ Assets/Code/CharacterAnim.cs
@@ -1,31 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class CharacterAnim : MonoBehaviour
 {
     public Animator animator;
-    int isWalkingHash, isRunningHash;
-    bool isRunning;
-    bool isWalking;
+    int isWalkingHash, isRunningHash, isJumpingHash, isAttackingHash;
 
     void Start()
     {
         //We get the ID of the required parameters — this way we will save time on searching for them
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
-        
+        isJumpingHash = Animator.StringToHash("isJumping");
+        isAttackingHash = Animator.StringToHash("isAttacking");
+
     }
 
     void Update()
     {
-        //We get their Boolean values from the parameter IDs
-        isRunning = animator.GetBool(isRunningHash);
-        isWalking = animator.GetBool(isWalkingHash);
+        // We get their Boolean values from the parameter IDs
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isJumping = animator.GetBool(isJumpingHash);
+        bool isAttacking = animator.GetBool(isAttackingHash);
+
 
         //We get Boolean values for the result of checking the player's input
         bool forwardPressed = Input.GetKey("w");
         bool runPressed = Input.GetKey("left shift");
+        bool jumpPressed = Input.GetKey("space");
+        bool attackClick = Input.GetMouseButtonDown(0);
+
 
         // If the player is not walking And the W key is pressed, then turn on the walking animation
         if (!isWalking && forwardPressed)
@@ -50,6 +58,30 @@ public class CharacterAnim : MonoBehaviour
         {
             animator.SetBool("isRunning", false);
         }
-    }
+        // If the player does not jump And the space bar is held down, then turn on the jump animation
+        if (!isJumping && jumpPressed)
+        {
+            animator.SetBool("isJumping", true);
+        }
 
+        // If the player jumps And the space bar is not held down, then turn off the jump animation
+        if (isJumping && !jumpPressed)
+        {
+            animator.SetBool("isJumping", false);
+        }
+
+        // If the player does not attack And pressed the left mouse button, then turn on the attack animation
+        if (!isAttacking && attackClick)
+        {
+            animator.SetBool("isAttacking", true);
+        }
+
+        // If the player attacks And has not pressed the left mouse button, then turn off the attack animation
+        if (isAttacking && !attackClick)
+        {
+            animator.SetBool("isAttacking", false);
+
+        }
+
+    }
 }
